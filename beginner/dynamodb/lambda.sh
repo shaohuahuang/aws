2aa35ebe7a82e62f7718c116ed99cf40249fec96
Shaohua Huang <hshnju@gmail.com>
2019-05-09 20:52:34 +0800

    script

--- beginner/dynamodb/lambda.sh
+++ beginner/dynamodb/lambda.sh
@@ -16,3 +16,13 @@ fi
 if [[ $1 = 'invoke' ]]; then
     aws lambda invoke --function-name ProcessDynamoDBRecords --payload file://input.txt outputfile.txt
 fi
+
+if [[ $1 = 'mapping' ]];then
+    aws lambda create-event-source-mapping --function-name ProcessDynamoDBRecords \
+    --batch-size 100 --starting-position LATEST --event-source arn:aws:dynamodb:ap-southeast-1:977462991389:table/lambda-dynamodb-stream/stream/2019-05-09T12:34:42.680
+fi
+
+
+if [[ $1 = 'delete-mapping' ]];then
+    aws lambda delete-event-source-mapping --uuid 542aceb1-0654-4d3c-9a3d-c1eba5e324d9
+fi
